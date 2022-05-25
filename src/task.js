07d391a5e49dde4488779ff4f211c8f20de54928
Rohit M <63762981+r0hitm@users.noreply.github.com>
2022-05-25 23:26:03 +0530

    Added description and priority field to Task

--- src/task.js
+++ src/task.js
@@ -10,32 +10,41 @@
  */
 
 //------ test data ------------
-const data = {
-    title: 'do something',
-    dueDate: '2022-6-22',
-}
+// const data = {
+//     title: 'do something',
+//     desc: 'important work for xyz',
+//     dueDate: '2022-6-22',
+//     priority: 'low'
+// }
 // -----------------------------
 
 class Task {
     constructor(data) {
         this.title = data.title;
+        this.desc = data.desc;
         this.dueDate = {
             year: Number.parseInt(data.dueDate.substr(0, 4)),
             month: Number.parseInt(data.dueDate.substr(5, 1)),
             day: Number.parseInt(data.dueDate.substr(7, 2)),
         }
+        this.priority = data.priority;
     }
 
     edit(data) {
         this.title = data.title;
+        this.desc = data.desc;
         this.dueDate = {
             year: Number.parseInt(data.dueDate.substr(0, 4)),
             month: Number.parseInt(data.dueDate.substr(5, 1)),
             day: Number.parseInt(data.dueDate.substr(7, 2)),
         }
+        this.priority = data.priority;
     }
 }
 
-const testing = new Task(data);
+// const testing = new Task(data);
 
-console.log(testing);
+// console.log(testing);
+
+
+export default Task;
